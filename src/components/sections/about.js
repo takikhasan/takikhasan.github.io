19a37682975c29727ba53014f1d071068d004e2a
Takik Hasan <hasantakik@gmail.com>
2022-06-04 02:38:46 +0600

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -136,10 +136,10 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Takik and I enjoy creating things that live on the internet. My
+              interest in programming started back in 2017 when I decided to try participating in
+              programming contests — turns out solving interesting problems taught me a lot
+              about Data Structures &amp; Algorithms!
             </p>
 
             <p>
