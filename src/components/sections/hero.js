5cf1cb423e21c49f54c5141d78fbc8bd6e505fc7
Takik Hasan <hasantakik@gmail.com>
2022-06-04 02:13:27 +0600

    Update hero.js

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,7 +59,7 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Takik Hasan.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
@@ -67,8 +67,8 @@ const Hero = () => {
         I’m a software engineer specializing in building (and occasionally designing) exceptional
         digital experiences. Currently, I’m focused on building accessible, human-centered products
         at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        <a href="https://enosisbd.com/" target="_blank" rel="noreferrer">
+          Enosis Solutions
         </a>
         .
       </p>
@@ -77,10 +77,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://codeforces.com/profile/Takik"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Check out my profile on Codeforces!
     </a>
   );
 
